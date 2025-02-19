e4298831819962632cf9512658deae4c062f512e
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-02-18 23:55:20 -0400

    implementacion del calculo de la fila y columna del tablero

---
+++ src/modules/components/BoardManipulator.vue
@@ -0,0 +1,30 @@
+<template>
+  <img
+    src="../../images/Tablero de Ajedrez_.png"
+    class="w-full absolute opacity-100"
+    alt="tablero"
+    ref="manipulatorTable"
+  />
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const manipulatorTable = ref<HTMLElement | null>(null);
+
+onMounted(() => manipulatorTable.value?.addEventListener('click', tablero));
+
+const tablero = (e: unknown) => {
+  /*evento del mouse para capturar ña posicion */
+  const y = e.layerY;
+  const x = e.layerX;
+
+  /*tamaño del tablero */
+  const heightScreen = e.target.clientHeight;
+  /* calculo de la posicion*/
+
+  const fila = Math.floor((y * 8) / heightScreen);
+  const columna = Math.floor((x * 8) / heightScreen);
+  console.log(columna, fila);
+};
+</script>
--- src/modules/components/BoardPieces.vue
+++ src/modules/components/BoardPieces.vue
@@ -1,94 +1,182 @@
 <template>
   <!-- blacks pieces -->
 
-  <img src="../../images/torre-negra.png" alt="torre negra" class="absolute left-0 w-1/8" />
-  <img src="../../images/caballo-negro.png" alt="caballo negro" class="absolute left-1/8 w-1/8" />
-  <img src="../../images/alfil-negro.png" alt="alfil negro" class="absolute left-2/8 w-1/8" />
-  <img src="../../images/Dama-negra.png" alt="dama negra" class="absolute left-3/8 w-1/8" />
-  <img src="../../images/rey-negro.png" alt="rey negro" class="absolute left-4/8 w-1/8" />
-  <img src="../../images/alfil-negro.png" alt="alfil negro" class="absolute left-5/8 w-1/8" />
-  <img src="../../images/caballo-negro.png" alt="caballo negro" class="absolute left-6/8 w-1/8" />
-  <img src="../../images/torre-negra.png" alt="torre negra" class="absolute left-7/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-0 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-1/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-2/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-3/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-4/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-5/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-6/8 top-1/8 w-1/8" />
-  <img src="../../images/peon-negro.png" alt="peon negro" class="absolute left-7/8 top-1/8 w-1/8" />
+  <img
+    @click="piece()"
+    src="../../images/torre-negra.png"
+    alt="torre negra"
+    class="absolute left-0 w-1/8 cursor-pointer"
+    ref="pieces"
+  />
+  <img
+    src="../../images/caballo-negro.png"
+    alt="caballo negro"
+    class="absolute left-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/alfil-negro.png"
+    alt="alfil negro"
+    class="absolute left-2/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/Dama-negra.png"
+    alt="dama negra"
+    class="absolute left-3/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/rey-negro.png"
+    alt="rey negro"
+    class="absolute left-4/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/alfil-negro.png"
+    alt="alfil negro"
+    class="absolute left-5/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/caballo-negro.png"
+    alt="caballo negro"
+    class="absolute left-6/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/torre-negra.png"
+    alt="torre negra"
+    class="absolute left-7/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-0 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-1/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-2/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-3/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-4/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-5/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-6/8 top-1/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-negro.png"
+    alt="peon negro"
+    class="absolute left-7/8 top-1/8 w-1/8 cursor-pointer"
+  />
 
   <!-- white pieces -->
   <img
     src="../../images/torre-blanca.png"
     alt="torre blanca"
-    class="absolute left-0 top-7/8 w-1/8"
+    class="absolute left-0 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/caballo-blanco.png"
     alt="caballo blanco"
-    class="absolute left-1/8 top-7/8 w-1/8"
+    class="absolute left-1/8 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/alfil-blanco.png"
     alt="alfil blanco"
-    class="absolute left-2/8 top-7/8 w-1/8"
+    class="absolute left-2/8 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/dama-blanca.png"
     alt="dama blanca"
-    class="absolute left-3/8 top-7/8 w-1/8"
+    class="absolute left-3/8 top-7/8 w-1/8 cursor-pointer"
   />
-  <img src="../../images/rey-blanco.png" alt="rey blanco" class="absolute left-4/8 top-7/8 w-1/8" />
+  <img
+    src="../../images/rey-blanco.png"
+    alt="rey blanco"
+    class="absolute left-4/8 top-7/8 w-1/8 cursor-pointer"
+  />
   <img
     src="../../images/alfil-blanco.png"
     alt="alfil blanco"
-    class="absolute left-5/8 top-7/8 w-1/8"
+    class="absolute left-5/8 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/caballo-blanco.png"
     alt="caballo blanco"
-    class="absolute left-6/8 top-7/8 w-1/8"
+    class="absolute left-6/8 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/torre-blanca.png"
     alt="torre blanca"
-    class="absolute left-7/8 top-7/8 w-1/8"
-  />
-  <img src="../../images/peon-blanco.png" alt="peon blanco" class="absolute left-0 top-6/8 w-1/8" />
-  <img
-    src="../../images/peon-blanco.png"
-    alt="peon blanco"
-    class="absolute left-1/8 top-6/8 w-1/8"
+    class="absolute left-7/8 top-7/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-2/8 top-6/8 w-1/8"
+    class="absolute left-0 top-6/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-3/8 top-6/8 w-1/8"
+    class="absolute left-1/8 top-6/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-4/8 top-6/8 w-1/8"
+    class="absolute left-2/8 top-6/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-5/8 top-6/8 w-1/8"
+    class="absolute left-3/8 top-6/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-6/8 top-6/8 w-1/8"
+    class="absolute left-4/8 top-6/8 w-1/8 cursor-pointer"
   />
   <img
     src="../../images/peon-blanco.png"
     alt="peon blanco"
-    class="absolute left-7/8 top-6/8 w-1/8"
+    class="absolute left-5/8 top-6/8 w-1/8 cursor-pointer"
   />
+  <img
+    src="../../images/peon-blanco.png"
+    alt="peon blanco"
+    class="absolute left-6/8 top-6/8 w-1/8 cursor-pointer"
+  />
+  <img
+    src="../../images/peon-blanco.png"
+    alt="peon blanco"
+    class="absolute left-7/8 top-6/8 w-1/8 cursor-pointer"
+  />
+
+  <!-- manipulador de las piezas -->
+  <BoardManipulator />
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import BoardManipulator from './BoardManipulator.vue';
+
+const pieces = ref<HTMLElement | null>(null);
+
+const piece = () => {
+  console.log('aqui');
+};
+</script>
