7f75883540daac8df619db97ff0bc1ec0f8d1794
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-02-26 00:30:37 -0400

    regla de comer peon enemigo completa

--- src/modules/rulesGames/pawns.ts
+++ src/modules/rulesGames/pawns.ts
@@ -11,6 +11,31 @@ export const whitePawns = (c: number, f: number, orderGame: GameManipulator[]) =
   };
   const array: RulesPieces[] = [];
 
+  /*encontrar pieza enemiga  izquierda*/
+
+  if (c - 1 >= 0 && f - 1 >= 0) {
+    const ei = orderGame.find((e) => e.left === c - 1 && e.top === f - 1);
+    if (ei?.piece.includes('black')) {
+      array.push({
+        top: f - 1,
+        left: c - 1,
+      });
+    }
+  }
+
+  /*encontrar pieza enemiga  derecha*/
+
+  if (c + 1 <= 7 && f - 1 >= 0) {
+    const ed = orderGame.find((e) => e.left === c + 1 && e.top === f - 1);
+
+    if (ed?.piece.includes('black')) {
+      array.push({
+        top: f - 1,
+        left: c + 1,
+      });
+    }
+  }
+
   if (f === 6) {
     for (let i = 0; i < 2; i++) {
       d.top--;
@@ -22,15 +47,21 @@ export const whitePawns = (c: number, f: number, orderGame: GameManipulator[]) =
           top: d.top,
           left: d.left,
         });
+      } else {
+        break;
       }
     }
     return array;
   }
   /*paso al frente */
-  array.push({
-    top: d.top - 1,
-    left: d.left,
-  });
+
+  const r = orderGame.find((e) => e.left === c && e.top === d.top - 1);
+  if (r?.piece === '') {
+    array.push({
+      top: d.top - 1,
+      left: d.left,
+    });
+  }
 
   return array;
 };
@@ -43,6 +74,30 @@ export const blackPawns = (c: number, f: number, orderGame: GameManipulator[]) =
 
   const array: RulesPieces[] = [];
 
+  /*encontrar pieza enemiga  izquierda*/
+
+  if (c - 1 >= 0 && f + 1 <= 7) {
+    const ei = orderGame.find((e) => e.left === c - 1 && e.top === f + 1);
+    if (ei?.piece.includes('white')) {
+      array.push({
+        top: f + 1,
+        left: c - 1,
+      });
+    }
+  }
+
+  /*encontrar pieza enemiga  derecha*/
+
+  if (c + 1 <= 7 && f + 1 <= 7) {
+    const ed = orderGame.find((e) => e.left === c + 1 && e.top === f + 1);
+
+    if (ed?.piece.includes('white')) {
+      array.push({
+        top: f + 1,
+        left: c + 1,
+      });
+    }
+  }
   if (f === 1) {
     for (let i = 0; i < 2; i++) {
       d.top++;
@@ -53,15 +108,21 @@ export const blackPawns = (c: number, f: number, orderGame: GameManipulator[]) =
           top: d.top,
           left: d.left,
         });
+      } else {
+        break;
       }
     }
     return array;
   }
+  /*paso al frente */
+  const r = orderGame.find((e) => e.left === c && e.top === d.top + 1);
 
-  array.push({
-    top: d.top + 1,
-    left: d.left,
-  });
+  if (r?.piece === '') {
+    array.push({
+      top: d.top + 1,
+      left: d.left,
+    });
+  }
 
   return array;
 };
