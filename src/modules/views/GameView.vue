f52fe92c63e5b2a3cb3ad58d635e92866ff0071a
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-03-20 18:53:45 -0400

    estilos del reloj

--- src/modules/views/GameView.vue
+++ src/modules/views/GameView.vue
@@ -5,11 +5,11 @@
     <main class="w-full h-auto flex justify-center">
       <div class="bg-gray-700 w-[90%]">
         <div v-bind:class="changeTurn">
-          <PlayerBlack v-bind:class="flip" :run="blackTurn" />
+          <PlayerBlack v-bind:class="flip" :run="blackTurn" @information="finalGame" />
 
           <BoardView @final="checkMate" :change="grados" />
 
-          <PlayerWhite v-bind:class="flip" :run="whiteTurn" />
+          <PlayerWhite v-bind:class="flip" :run="whiteTurn" @information="finalGame" />
         </div>
       </div>
     </main>
@@ -34,7 +34,7 @@ import PlayerWhite from '../components/PlayerWhite.vue';
 
 const grados = ref<number>(0);
 const changeTurn = ref<string>(
-  `rotate-${grados.value} flex flex-col m-auto items-center bg-gray-400 max-w-[32rem]`,
+  `rotate-${grados.value} flex flex-col m-auto h-auto items-center bg-gray-400 max-w-[32rem] gap-0.5`,
 );
 const final = ref<boolean>(false);
 const colorWin = ref<string | null>(null);
@@ -52,13 +52,13 @@ const checkMate = (f: boolean, color: string | null) => {
       if (color === 'Blancas') {
         grados.value = 180;
         flip.value = 'rotate-180 duration-700';
-        changeTurn.value = `rotate-180 duration-700 flex flex-col m-auto items-center bg-gray-400 max-w-[32rem]`;
+        changeTurn.value = `rotate-180 duration-700 flex flex-col m-auto items-center bg-gray-400 max-w-[32rem] gap-0.5`;
         blackTurn.value = true;
         whiteTurn.value = false;
       } else {
         grados.value = 360;
         flip.value = 'rotate-360 duration-700';
-        changeTurn.value = `rotate-360 duration-700 flex flex-col m-auto items-center bg-gray-400 max-w-[32rem]`;
+        changeTurn.value = `rotate-360 duration-700 flex flex-col m-auto items-center bg-gray-400 max-w-[32rem] gap-0.5`;
         whiteTurn.value = true;
         blackTurn.value = false;
       }
@@ -68,5 +68,10 @@ const checkMate = (f: boolean, color: string | null) => {
 
 const closeModal = (d: boolean) => {
   final.value = d;
+};
+
+const finalGame = (f: boolean, c: string) => {
+  final.value = f;
+  colorWin.value = c;
 };
 </script>
